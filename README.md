ed9e56233c24bfe3ed9f5d931ff6bedbce38f40a
dxyinme <dxyinme@outlook.com>
2024-04-07 22:50:51 +0800

    update must test case and readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# XERR
+
+[![Testing](https://github.com/lukaproject/xerr/actions/workflows/testing.yml/badge.svg?branch=main)](https://github.com/lukaproject/xerr/actions/workflows/testing.yml)
+[![codecov](https://codecov.io/gh/lukaproject/xerr/graph/badge.svg?token=lmEJX57An5)](https://codecov.io/gh/lukaproject/xerr)
+
+
+A golang error handling style.
--- must_test.go
+++ must_test.go
@@ -2,23 +2,67 @@ package xerr_test
 
 import (
 	"errors"
+	"runtime/debug"
 	"testing"
 
 	"github.com/lukaproject/xerr"
 )
 
+func recoverT(t *testing.T) {
+	if err := recover(); err == nil {
+		t.Fatal(err)
+	} else {
+		t.Log(string(debug.Stack()))
+		t.Log(err)
+	}
+}
+
 func TestMust(t *testing.T) {
 	t.Run("test error panic ok", func(t *testing.T) {
-		defer func() {
-			if err := recover(); err == nil {
-				t.Fatal(err)
-			} else {
-				t.Log(err)
-			}
-		}()
+		defer recoverT(t)
 		hasErr := func() (int, error) {
 			return 0, errors.New("test")
 		}
 		xerr.Must(hasErr())
+	})
+
+	t.Run("test error no panic", func(t *testing.T) {
+		hasErr := func() (int, error) {
+			return 1, nil
+		}
+		v := xerr.Must(hasErr())
+		if v != 1 {
+			t.Fatalf("v != 1, v = %d", v)
+		}
+	})
+}
+
+func TestMust0(t *testing.T) {
+	t.Run("test error panic ok", func(t *testing.T) {
+		defer recoverT(t)
+		hasErr := func() error {
+			return errors.New("test")
+		}
+		xerr.Must0(hasErr())
+	})
+}
+
+func TestMus2(t *testing.T) {
+	t.Run("test error panic ok", func(t *testing.T) {
+		defer recoverT(t)
+		hasErr := func() (string, int, error) {
+			return "", 1, errors.New("test")
+		}
+		xerr.Must2(hasErr())
+	})
+
+	t.Run("test error no panic", func(t *testing.T) {
+		hasErr := func() (string, int, error) {
+			return "123", 1, nil
+		}
+		str, val := xerr.Must2(hasErr())
+		if str != "123" || val != 1 {
+			t.Fatalf("str = [%s], val = [%d]", str, val)
+		}
 	})
 }
