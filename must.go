d12bd2b3a6fcf3c11ef3ffcf4eff44cbceabe8b6
Dxyinme <32793868+dxyinme@users.noreply.github.com>
2024-06-22 16:07:14 +0800

    add must ok function. (#2)
    * update must ok
    
    * update must ok

--- must.go
+++ must.go
@@ -1,5 +1,11 @@
 package xerr
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+)
+
 // Because func(...) (T, error) is the most one
 // error return type in go project, So we named
 // it `Must` as the most easily to use.
@@ -28,4 +34,19 @@ func Must2[T0, T1 any](v0 T0, v1 T1, err error) (T0, T1) {
 		panic(err)
 	}
 	return v0, v1
+}
+
+// MustOk
+// If the ok is true, return v.(T)
+// otherwise panic.
+// you can use this function in get a map value.
+func MustOk[T any](v any, ok bool) T {
+	if ok {
+		res, castOk := v.(T)
+		if !castOk {
+			panic(fmt.Errorf("can't cast v[%s]", reflect.TypeOf(v).String()))
+		}
+		return res
+	}
+	panic(errors.New("not ok"))
 }
